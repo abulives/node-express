5d17a7963d3dcb5612fcc5127cb5c93daba7e418
abu <abulives@gmail.com>
2019-08-26 12:18:03 +0530

    validation

--- app.js
+++ app.js
@@ -34,9 +34,6 @@ app.use(function(req, res, next) {
   next();
 });
 app.use('/api/v1/users', usersRouter);
-app.all('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
